48aff9d26634d15e37ada4901d65738d4b45f8e2
mail@alfnilsson.se <mail@alfnilsson.se>
2017-12-03 22:09:45 +0100

    Visual Tests from Partner Close Up 2017

---
+++ EpiCloseUp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EpiCloseUp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EpiCloseUp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6985c473-1841-4e4b-a70b-a02ab82b9b1d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EpiCloseUp.Tests/VisualTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using Applitools.Selenium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+
+namespace EpiCloseUp.Tests
+{
+    [TestClass]
+    public class VisualTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void TestAlloy()
+        {
+            using (IWebDriver driver = new PhantomJSDriver())
+            {
+                driver.Manage().Window.Size = new Size(1280, 980);
+                var eyes = new Eyes
+                {
+                    ApiKey = "--APIKEY--"
+                };
+
+                try
+                {
+                    driver.Url = "http://epicloseup2017.azurewebsites.net/";
+                    eyes.CheckWindow("Start Page");
+
+                    driver.Url = "http://epicloseup2017.azurewebsites.net/en/about-us/contact-us/";
+                    eyes.CheckWindow("Contact us - Forms");
+
+                    driver.FindElement(FormPage.SubmitButton).Click();
+                    eyes.CheckWindow("Contact us - Missing fields");
+
+                    driver.FindElement(FormPage.NameField).SendKeys("Alf Nilsson");
+                    driver.FindElement(FormPage.EmailField).SendKeys("alf.nilsson");
+                    driver.FindElement(FormPage.SubmitButton).Click();
+                    eyes.CheckWindow("Contact us - Invalid e-mail");
+
+                    driver.FindElement(FormPage.EmailField).SendKeys("@netrelations.com");
+                    driver.FindElement(FormPage.AlloyMeetCheckbox).Click();
+                    driver.FindElement(FormPage.PricingCheckbox).Click();
+                    driver.FindElement(FormPage.MessageField).SendKeys("Oh sweet mama!");
+
+                    eyes.CheckWindow("Contact us - Form entered");
+
+                    driver.FindElement(FormPage.SubmitButton).Click();
+                    eyes.CheckWindow("Contact us - Confirmation");
+                    eyes.Close();
+                }
+                catch (Exception ex)
+                {
+                    Assert.Fail(ex.Message);
+                }
+                finally
+                {
+                    eyes.AbortIfNotClosed();
+                }
+            }
+        }
+    }
+
+    public static class FormPage
+    {
+        public static By SubmitButton { get; } = By.XPath("//input[@name='XFormsSubmit_run68XpSP4HHFwPPbtDwSoLowJFfcqSLO4vdDKV0Q']");
+        public static By NameField { get; } = By.XPath("//input[@name='Name']");
+        public static By EmailField { get; } = By.XPath("//input[@name='Email']");
+        public static By AlloyMeetCheckbox { get; } = By.XPath("//input[@name='AreaofInterestAlloy Meet']");
+        public static By PricingCheckbox { get; } = By.XPath("//input[@name='AreaofInterestPricing']");
+        public static By MessageField { get; } = By.XPath("//textarea[@name='Message']");
+    }
+}
